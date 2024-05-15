674244e9b7a0dea74abf91aa20c3b0311cdd3eb4
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-05-15 16:51:22 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+//პროექტი 10 - ქულების გამომთვლელი
+//შექმენით მარტივი JavaScript პროგრამა, რომელიც ითვლის შეფასებას მოსწავლის ქულების მიხედვით.
+//შეფასების სქემა შემდეგნაირია:
+//A: 90-100 B: 80-89 C: 70-79 D: 60-69 F: 0-59
+// პროგრამამ უნდა მოსთხოვოს მომხმარებელს შეიყვანოს რიცხვითი ქულა(0-დან 100-ის ჩათვლით ნებისმიერი რიცხვი).
+// გამოიყენეთ პირობითი ოპერატორები შეყვანილი რიცხვის შეფასების დასადგენად.
+// ეკრანზე გამოიტანეთ როგორც შეყვანილი რიცხვი ასევე შესაბამისი ასო.
+//მაგალითად:shell> Enter the student's score: 78 > Student's Score: 78 > Grade: C
+// const mark = Number(prompt('Enter the student\'s score'))
+// let grade = ''
+// if(mark >= 90 && mark <= 100){
+//     grade = 'grade: A'
+//     console.log(`student's score is ${mark}, ${grade}`,)
+// }else if(mark >= 80 && mark <=89){
+//     grade = 'grade: B'
+//     console.log(`student's score is ${mark}, ${grade}`,)
+// }else if(mark >= 70 && mark <=79){
+//     grade = 'grade: C'
+//     console.log(`student's score is ${mark}, ${grade}`,)
+// }else if(mark >= 60 && mark <=69){
+//     grade = 'grade: D'
+//     console.log(`student's score is ${mark}, ${grade}`,)
+// }else if(mark >= 0 && mark <=59){
+//     grade = 'grade: F'
+//     console.log(`student's score is ${mark}, ${grade}`,)
+// }else{
+//     console.log('enter the number between 0 and 100')
+// }
+
+
+    // პროექტი 11 - Simple Math - მარტივი მათემატიკა
+    //თქვენ ხშირად დაწერთ პროგრამებს, რომლებიც ეხება რიცხვებს. ხშირად, პროგრამაში
+    // მიღებული მონაცემები (input )რიცხვით მონაცემების ტიპებად უნდა გარდაქმნათ. დაწერეთ პროგრამა,
+    // რომელიც მოგთხოვთ ორ რიცხვს. ამობეჭდეთ ამ რიცხვების ჯამი, სხვაობა, ნამრავლი და განაყოფი.
+    //მაგალითად:
+    //shell
+    // What is the first number? 10
+    // What is the second number? 5
+    // 10 + 5 = 15
+    // 10 - 5 = 5
+    // 10 * 5 = 50
+    // 10 / 5 = 2
+// let number1 = Number(prompt('what is the first number?'))
+// let number2 = Number(prompt('what is the second number?'))
+// console.log( `${number1} + ${number2} = `,number1 + number2, )
+// console.log( `${number1} - ${number2} = `,number1 - number2, )
+// console.log( `${number1} * ${number2} = `,number1 * number2, )
+// console.log( `${number1} / ${number2} = `,number1 / number2, )
+
